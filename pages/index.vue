90d55abd39e35a3d046bcd9b98e552e2603afba8
Michael <michael@robertsmj.com>
2020-04-06 12:40:41 -0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="container">
+
+    </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+    components: {
+        Logo
+    },
+    head() {
+        return {
+                script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+        };
+    },
+}
+</script>
+
+<style>
+
+.container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+</style>
